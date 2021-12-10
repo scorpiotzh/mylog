2e44458c5f6591eb9088cfbcb4a596cc093150d8
scorpiotzh <835598264@qq.com>
2021-12-10 16:36:57 +0800

    update log

--- log_test.go
+++ log_test.go
@@ -3,10 +3,10 @@ package mylog
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
-func TestLog(t *testing.T) {
-	//InitMyLog(nil)
+func TestNewLogger(t *testing.T) {
 	log := NewLogger("test", LevelDebug)
 
 	log.Debug("aaaaa")
@@ -29,4 +29,30 @@ func TestLog(t *testing.T) {
 	//log.Fatalf("aaa %s aaa","bbb")
 	//fmt.Println("cccc")
 	//fmt.Println("aaaaa")
+}
+
+func TestNewLoggerDefault(t *testing.T) {
+	log := NewLoggerDefault("test", LevelDebug, nil)
+
+	log.Debug("aaaaa")
+	log.Debugf("aaa %s aaa", "bbb")
+
+	log.Info("aaaaa")
+	log.Infof("aaa %s aaa", "bbb")
+
+	log.Warn("aaaaa")
+	log.Warnf("aaa %s aaa", "bbb")
+
+	log.Error("aaaaa")
+	log.Errorf("aaa %s aaa", "bbb")
+
+	fmt.Println("a")
+
+	//log.Panic("aaaaa")
+	//fmt.Println("b")
+
+	log.Fatalf("aaa %s aaa", "bbb")
+	fmt.Println("cccc")
+	fmt.Println("aaaaa")
+	time.Sleep(time.Second)
 }
